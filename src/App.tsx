88c9b1e24b421b819d0eda5b7278fd47d85b54c0
ri4an <vadimguber7@gmail.com>
2022-09-23 17:20:20 +0600

    init app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './pages/Header'
+import MainPage from './pages/MainPage'
+
+function App() {
+	return (
+		<div id='app'>
+			<Header />
+			<MainPage />
+		</div>
+	)
+}
+
+export default App
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import Items from '../components/items/Items'
+import Search from '../components/Search'
+
+const MainPage = () => {
+	return (
+		<div>
+			<Search />
+			<Items />
+		</div>
+	)
+}
+export default MainPage
